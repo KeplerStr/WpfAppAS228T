c6eae09defcbffb9fbe7f493acb9a178ceff53b7
hRowe <caiwei_filove@163.com>
2021-10-29 17:35:51 +0800

    tds

---
+++ WpfAppAS228T/Common/CommandBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfAppAS228T.Common
+{
+    public class CommandBase : ICommand
+    {
+        //public event EventHandler CanExecuteChanged;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return DoCanExecute?.Invoke(parameter) == true;
+            //throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            DoExecute?.Invoke(parameter);
+            //throw new NotImplementedException();
+        }
+
+        public Action<object> DoExecute { get; set; }
+
+        public Func<object,bool> DoCanExecute { get; set; }
+        
+        //public Func<object,bool> DoCanExecute { get; set; }
+    }
+}
---
+++ WpfAppAS228T/ViewModel/LoginViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfAppAS228T.Common;
+using WpfAppAS228T.Model;
+
+namespace WpfAppAS228T.ViewModel
+{
+    public class LoginViewModel
+    {
+        public CommandBase CloseWindowCommand { get; set; }
+
+        public LoginModel LoginModel { get; set; }
+
+        public LoginViewModel()
+        {
+            this.LoginModel = new LoginModel();
+
+            this.LoginModel.UserName = "kepler";
+            this.LoginModel.Password = "123456";
+
+            
+            this.CloseWindowCommand = new CommandBase();
+            this.CloseWindowCommand.DoExecute = new Action<object>((o) => 
+            {
+                (o as Window).Close();
+            });
+
+
+            this.CloseWindowCommand.DoCanExecute = new Func<object, bool>((o) => 
+            {
+                return true;
+            });
+
+        }
+    }
+}
