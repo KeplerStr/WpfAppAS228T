ce3e5bb34efde56bf5f5d0822bb6941a704a9a75
hRowe <caiwei_filove@163.com>
2021-11-02 08:55:21 +0800

    添加MainViewModel.cs以及NMODEBUS

---
+++ WpfAppAS228T/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppAS228T.Common;
+
+namespace WpfAppAS228T.ViewModel
+{
+    class MainViewModel : NotifyBase
+    {
+
+    }
+}
